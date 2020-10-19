63e2600a843fe1ff36ada0ff38814c103fb2eb91
chojana <adam.chojan@snpgroup.com>
2020-10-19 13:31:07 +0200

    Commnents Change layout to RelativeLayout Additional changes that could help with missing content

--- app/src/main/java/com/example/snpserviceapp/MainActivity.java
+++ app/src/main/java/com/example/snpserviceapp/MainActivity.java
@@ -3,36 +3,47 @@ package com.example.snpserviceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView mywebView;
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        mywebView = (WebView) findViewById(R.id.webview);
-        WebSettings webSettings= mywebView.getSettings();
+        //find webView element in activity
+        myWebView = (WebView) findViewById(R.id.webview);
+        //set chrome as webview client In some cases this should help with missing content
+        myWebView.setWebChromeClient(new WebChromeClient());
+        WebSettings webSettings= myWebView.getSettings();
+        //Enable javascirpt, needed at least for Microsoft Sing-in
         webSettings.setJavaScriptEnabled(true);
-        //mywebView.loadUrl("https://example.com/");
-        mywebView.loadUrl("https://snpcom.sharepoint.com/GlobalFunctions/administration/SNP%20Service%20APP/SiteAssets/scripts/index.aspx");
-        mywebView.setWebViewClient(new WebViewClient());
+
+        //In some cases this should help with missing content -> it didn't
+        webSettings.setDomStorageEnabled(true);
+
+        //In other cases this should help with missing content-> it didn't
+//        webSettings.setLoadWithOverviewMode(true);
+//        webSettings.setUseWideViewPort(true);
+//        webSettings.setBuiltInZoomControls(true);
+//        webSettings.setDisplayZoomControls(false);
+//        webSettings.setSupportZoom(true);
+//        webSettings.setDefaultTextEncodingName("utf-8");
+        myWebView.loadUrl("https://snpcom.sharepoint.com/GlobalFunctions/administration/SNP%20Service%20APP/SiteAssets/scripts/index.aspx");
+        myWebView.setWebViewClient(new WebViewClient());
     }
 
     @Override
     public void onBackPressed() {
-        if(mywebView.canGoBack())
-        {
-            mywebView.goBack();
-        }
-
-        else{
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else{
             super.onBackPressed();
         }
     }
