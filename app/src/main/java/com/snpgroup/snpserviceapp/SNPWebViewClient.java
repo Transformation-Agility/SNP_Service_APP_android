28a95882197f523d1546192f24542a145feec21c
chojana <adam.chojan@snpgroup.com>
2020-10-28 13:31:08 +0100

    Open Teams chat handling

--- app/src/main/java/com/snpgroup/snpserviceapp/SNPWebViewClient.java
+++ app/src/main/java/com/snpgroup/snpserviceapp/SNPWebViewClient.java
@@ -3,7 +3,9 @@ package com.snpgroup.snpserviceapp;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.MailTo;
+import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import java.lang.ref.WeakReference;
@@ -16,14 +18,24 @@ public class SNPWebViewClient extends WebViewClient {
     }
 
     @Override
-    // specifically handle mailto-links
+    // specifically handle mailto-links and teams chat
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        final Activity activity = mActivityRef.get();
         if (url.startsWith("mailto:")) {
-            final Activity activity = mActivityRef.get();
             if (activity != null) {
                 MailTo mt = MailTo.parse(url);
                 Intent i = newEmailIntent(activity, mt.getTo(), mt.getSubject(), mt.getBody(), mt.getCc());
                 activity.startActivity(i);
+                view.reload();
+                return true;
+            }
+        } else if (url.startsWith("sip:")) {
+            PackageManager packageManager = activity.getPackageManager();
+
+            Intent sendIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("msteams://teams.microsoft.com/l/chat/0/0?users=" + url.substring(4)));
+            if (sendIntent.resolveActivity(packageManager) != null) {
+                activity.startActivity(sendIntent);
                 view.reload();
                 return true;
             }
